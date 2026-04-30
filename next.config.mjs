a45483d17908fa763ff4d81df67dabbbf62dd100
Ali Mohammadnia <alimohammadnia127@gmail.com>
2026-04-30 23:57:08 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,36 @@
+// import createNextIntlPlugin from 'next-intl/plugin';
+
+// const withNextIntl = createNextIntlPlugin('./i18n.ts');
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+// }
+
+// export default withNextIntl(nextConfig)
+
+
+/** @type {import('next').NextConfig} */
+const isExport = process.env.NEXT_EXPORT === 'true'
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+  output: isExport ? 'export' : 'standalone',
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
+  ...(basePath ? { basePath, assetPrefix: basePath } : {}),
 }
 
-export default withNextIntl(nextConfig)
+export default nextConfig
