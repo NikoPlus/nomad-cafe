bbacd4ee733c9535a89f39868b5b2e7fa6f9f485
Ali Mohammadnia <alimohammadnia127@gmail.com>
2026-05-05 23:50:01 +0330

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,15 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 /** @type {import('next').NextConfig} */
-const isExport = process.env.NEXT_EXPORT === 'true'
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
+const isExport = process.env.NEXT_EXPORT === 'true';
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '/nomad-cafe'; // Default to '/nomad-cafe' for GitHub Pages
 
 const nextConfig = {
-  output: isExport ? 'export' : 'standalone',
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  images: { unoptimized: true },
-  ...(basePath ? { basePath, assetPrefix: basePath } : {}),
-}
+  output: isExport ? 'export' : 'standalone', // Use 'export' for static export on GitHub Pages
+  eslint: { ignoreDuringBuilds: true }, // Ignore ESLint errors during build
+  typescript: { ignoreBuildErrors: true }, // Ignore TypeScript errors during build
+  images: { unoptimized: true }, // Disable optimized images for static export
+  ...(basePath ? { basePath, assetPrefix: basePath } : {}), // Base path and asset prefix for GitHub Pages URLs
+};
 
-export default withNextIntl(nextConfig)
+export default withNextIntl(nextConfig);
