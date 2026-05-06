5d390fe57c71399d9874adc31b0e0c5bbc5f1a0a
Ali <alimohammadnia127@gmail.com>
2026-05-06 06:44:05 +0330

    fix the next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,11 @@ const isExport = process.env.NEXT_EXPORT === 'true';
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '/nomad-cafe'; // Default to '/nomad-cafe' for GitHub Pages
 
 const nextConfig = {
-  output: 'export',
-  // output: isExport ? 'export' : 'standalone', // Use 'export' for static export on GitHub Pages
+  // output: 'export',
+  output: isExport ? 'export' : 'standalone', // Use 'export' for static export on GitHub Pages
   typescript: { ignoreBuildErrors: true }, // Ignore TypeScript errors during build
   images: { unoptimized: true }, // Disable optimized images for static export
-  ...(basePath ? { basePath, assetPrefix: basePath } : {}), // Base path and asset prefix for GitHub Pages URLs
+  // ...(basePath ? { basePath, assetPrefix: basePath } : {}), // Base path and asset prefix for GitHub Pages URLs
 };
 
 export default withNextIntl(nextConfig);
